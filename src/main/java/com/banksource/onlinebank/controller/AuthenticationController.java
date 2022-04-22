1e04269f827a730f3c27c4dcc4abef20d1042b28
konst <qwertyuiop12345jomh@gmail.com>
2022-04-22 16:23:46 +0300

    Completed API added decryption at abstractController

--- src/main/java/com/banksource/onlinebank/controller/AuthenticationController.java
+++ src/main/java/com/banksource/onlinebank/controller/AuthenticationController.java
@@ -2,26 +2,21 @@ package com.banksource.onlinebank.controller;
 
 import com.banksource.onlinebank.components.User;
 import com.banksource.onlinebank.service.mainServices.userService;
-import com.banksource.onlinebank.work.Headers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping(path="/login", produces="application/json")
 public class AuthenticationController {
 
     private com.banksource.onlinebank.service.mainServices.userService userService;
-    private Headers headers;
 
     @Autowired
-    public AuthenticationController(userService userService, Headers headers){
+    public AuthenticationController(userService userService){
         this.userService = userService;
-        this.headers = headers;
     }
 
     @GetMapping
@@ -29,12 +24,12 @@ public class AuthenticationController {
         try{
             User user =  userService.findUser(authentication.getName());
             if(user != null){
-                return ResponseEntity.ok().headers(headers.getHeaders()).body(HttpStatus.OK);
+                return ResponseEntity.ok().body("{data: " + HttpStatus.OK + "}");
             }
-            return ResponseEntity.notFound().headers(headers.getHeaders()).build();
+            return ResponseEntity.notFound().build();
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
-            return ResponseEntity.internalServerError().headers(headers.getHeaders()).body(exception.getMessage());
+            return ResponseEntity.internalServerError().body(exception.getMessage());
         }
     }
 }
